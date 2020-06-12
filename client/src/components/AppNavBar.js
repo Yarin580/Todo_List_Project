5769d8faf02bf1d1b4d7b05c90aa85b5713451aa
Yarin Hershko <yarin5805180@gmail.com>
2020-06-12 10:22:43 +0300

    work with userID and not with all users info + add loading animation

--- client/src/components/AppNavBar.js
+++ client/src/components/AppNavBar.js
@@ -1,10 +1,26 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Navbar, NavbarBrand, Nav, NavItem } from "reactstrap";
 import { UserContext } from "../context/UserContext";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 function AppNavBar() {
   const { userLogin, setUserLogin } = useContext(UserContext);
+
+  const [fullName, setFullname] = useState();
+
+  useEffect(() => {
+    getFullName();
+  });
+
+  function getFullName() {
+    axios
+      .get(`/users/fullname/${userLogin}`)
+      .then((res) => {
+        setFullname(res.data);
+      })
+      .catch((err) => console.log(err));
+  }
 
   return (
     <div>
@@ -14,34 +30,51 @@ function AppNavBar() {
         expand="md"
         className="mb-5"
       >
-        {userLogin ? (
-          <NavbarBrand>
-            <h4>hello {userLogin.firstName} </h4>
-          </NavbarBrand>
-        ) : null}
-
-        <NavbarBrand className="ml-auto">
+        <NavbarBrand style={{ alignItems: "center" }}>
           <Link style={{ color: "white" }} to="/">
             <h1>ToDo App</h1>
           </Link>
         </NavbarBrand>
+        {userLogin ? (
+          <NavbarBrand>
+            <h4>hello {fullName} </h4>
+          </NavbarBrand>
+        ) : null}
 
         <Nav className="ml-auto" navbar>
-          <NavItem>
-            {userLogin ? (
-              <Link
-                style={{ color: "white" }}
-                onClick={() => setUserLogin(null)}
-                to="/"
-              >
-                logOut
-              </Link>
+          {userLogin ? (
+            userLogin.is_admin ? (
+              <NavItem>
+                <Link style={{ color: "white" }} to="/AdminHomePage">
+                  admin page
+                </Link>
+                &nbsp; &nbsp;
+                <Link
+                  style={{ color: "white" }}
+                  onClick={() => setUserLogin(null)}
+                  to="/"
+                >
+                  logOut
+                </Link>
+              </NavItem>
             ) : (
+              <NavItem>
+                <Link
+                  style={{ color: "white" }}
+                  onClick={() => setUserLogin(null)}
+                  to="/"
+                >
+                  logOut
+                </Link>
+              </NavItem>
+            )
+          ) : (
+            <NavItem>
               <Link style={{ color: "white" }} to="/Login">
                 login/Signup
               </Link>
-            )}
-          </NavItem>
+            </NavItem>
+          )}
         </Nav>
       </Navbar>
     </div>
--- client/src/components/TodoList/TodoList.js
+++ client/src/components/TodoList/TodoList.js
@@ -1,27 +1,33 @@
 import React, { useState, useEffect, useContext } from "react";
-import { ListGroup, ListGroupItem, Button, Input } from "reactstrap";
+import { ListGroup, ListGroupItem, Button, Input, Spinner } from "reactstrap";
 import axios from "axios";
 import { UserContext } from "../../context/UserContext";
+import "../../App.css";
 
 function TodoList(props) {
   const [todoList, setTodoList] = useState({
     todos: [],
   });
 
+  const [loading, setLoading] = useState(true);
+
   //get the userID from the context
   const { userLogin } = useContext(UserContext);
 
   //when the component show up => call to getTodos function
   useEffect(() => {
-    if (userLogin) getTodos();
+    setTimeout(() => {
+      if (userLogin) getTodos();
+    }, 500);
   });
 
   function getTodos() {
     //get only the todos with the same userID ant put them on todos state
     axios
-      .get(`/todos/${userLogin._id}`)
+      .get(`/todos/${userLogin}`)
       .then((res) => {
         setTodoList({ todos: res.data });
+        setLoading(false);
       })
       .catch(() => {
         alert("somthing goes worng");
@@ -46,34 +52,53 @@ function TodoList(props) {
     <div>
       {userLogin ? (
         <ListGroup>
-          {todoList.todos.map((todo) => (
-            <ListGroupItem key={todo._id}>
-              <Input
-                type="checkbox"
-                size="md"
-                style={{ float: "left" }}
-                onChange={() => isDoneHandler(todo._id)}
-              />
-              <span
+          {loading ? (
+            <span
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Spinner
                 style={{
-                  textDecoration: todo.is_done ? "line-through" : "none",
+                  width: "10rem",
+                  height: "10rem",
                 }}
-              >
-                {todo.value}
-              </span>
+              />
+            </span>
+          ) : (
+            todoList.todos.map((todo) => (
+              <ListGroupItem key={todo._id}>
+                <Input
+                  type="checkbox"
+                  size="md"
+                  style={{ float: "left" }}
+                  checked={todo.is_done}
+                  onClick={() => isDoneHandler(todo._id)}
+                  toog
+                />
+                <span
+                  style={{
+                    textDecoration: todo.is_done ? "line-through" : "none",
+                  }}
+                >
+                  {todo.value}
+                </span>
 
-              <Button
-                color="danger"
-                style={{ float: "right" }}
-                onClick={() => deleteHandler(todo._id)}
-              >
-                DELETE
-              </Button>
-            </ListGroupItem>
-          ))}
+                <Button
+                  color="danger"
+                  style={{ float: "right" }}
+                  onClick={() => deleteHandler(todo._id)}
+                >
+                  DELETE
+                </Button>
+              </ListGroupItem>
+            ))
+          )}
         </ListGroup>
       ) : (
-        <h1>you need to log in</h1>
+        <h1 style={{ textAlign: "center" }}>you need to log in</h1>
       )}
     </div>
   );
