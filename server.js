02027f316b9b6e80cd5526c6ee8207ef2d2e9239
Yarin Hershko <yarin5805180@gmail.com>
2020-06-14 12:05:18 +0300

    server update

--- server.js
+++ server.js
@@ -25,5 +25,6 @@ app.use("/todos", Todos);
 const Users = require("./routes/Users_Requests");
 app.use("/users", Users);
 
+const port = process.env.PORT || 5000;
 // starting the server on port 5000
-app.listen(5000, () => console.log("server start on port 5000"));
+app.listen(port, () => console.log(`server start on port ${port}`));
